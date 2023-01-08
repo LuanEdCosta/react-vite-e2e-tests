47178b6227cb3ffebd5d80b29ca6362705aa6b12
Luan Eduardo <luan1346@gmail.com>
2023-01-07 23:31:49 -0300

    Add e2e tests

---
+++ e2e/task.e2e.test.ts
@@ -0,0 +1,62 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:5173");
+});
+
+test.describe("Todo page tests", () => {
+  test("create a task and delete it", async ({ page }) => {
+    const task = "my new task";
+
+    await page.locator("input").click();
+    await page.locator("input").fill(task);
+    await page.getByRole("button", { name: "Create Task" }).click();
+
+    await expect(page.locator("ul>li")).toHaveCount(1);
+    await expect(page.getByText(task)).toBeVisible();
+
+    await page.getByRole("button", { name: "x" }).click();
+
+    await expect(page.locator("ul>li")).toHaveCount(0);
+    await expect(page.locator("ul")).not.toBeVisible();
+  });
+
+  test("create multiple tasks", async ({ page }) => {
+    const task1 = "first task";
+    const task2 = "second task";
+
+    await page.locator("input").click();
+    await page.locator("input").fill(task1);
+    await page.getByRole("button", { name: "Create Task" }).click();
+
+    await page.locator("input").click();
+    await page.locator("input").fill(task2);
+    await page.getByRole("button", { name: "Create Task" }).click();
+
+    await expect(page.locator("ul>li")).toHaveCount(2);
+    await expect(page.getByText(task1)).toBeVisible();
+    await expect(page.getByText(task2)).toBeVisible();
+  });
+
+  test("focus input after creating task", async ({ page }) => {
+    const task = "new task";
+
+    await expect(page.locator("input")).not.toBeFocused();
+    await page.locator("input").click();
+    await page.locator("input").fill(task);
+    await expect(page.locator("input")).toBeFocused();
+    await page.getByRole("button", { name: "Create Task" }).click();
+    await expect(page.locator("input")).toBeFocused();
+  });
+
+  test("create a task with enter key", async ({ page }) => {
+    const task = "task with enter";
+
+    await page.locator("input").click();
+    await page.locator("input").fill(task);
+    await page.locator("input").press("Enter");
+
+    await expect(page.locator("ul>li")).toHaveCount(1);
+    await expect(page.getByText(task)).toBeVisible();
+  });
+});
